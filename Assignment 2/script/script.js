e25d6408d5ca67d7a6dbb0c16f97a64df4026ba7
patchell-c <patchell.catherine@gmail.com>
2014-10-17 17:33:52 -0400

    fix axes display

--- Assignment 2/script/script.js
+++ Assignment 2/script/script.js
@@ -1,8 +1,11 @@
 //global vars
 var min_literacy, max_literacy, min_life, max_life = 0;
+var x;
+var y;
+var radius_scale;
 
 //Determine the size of the plot, as well as the margins
-var margin = {t:150,r:150,b:400,l:150};
+var margin = {t:150,r:150,b:300,l:150};
 var width = $('.canvas').width() - margin.l - margin.r;
 var height = $('.canvas').height() - margin.t - margin.b;
 
@@ -27,7 +30,7 @@ d3.csv(
       literacy: (d["Literacy rate, adult total (% of people ages 15 and above)"] == "..") ? undefined: +d["Literacy rate, adult total (% of people ages 15 and above)"],
       malnutrition: (d["Malnutrition prevalence, weight for age (% of children under 5)"] == "..") ? undefined: +d["Malnutrition prevalence, weight for age (% of children under 5)"],
       hospitalBeds: (d["Hospital beds (per 1,000 people)"] == "..") ? undefined: +d["Hospital beds (per 1,000 people)"],
-      healthExp: (d["Health expenditure, total (% of GDP)"] == "..") ? undefined: +d["Health expenditure, total (% of GDP)"],
+      healthExpense: (d["Health expenditure, total (% of GDP)"] == "..") ? undefined: +d["Health expenditure, total (% of GDP)"],
     };
   },
   function(error,rows){
@@ -46,47 +49,64 @@ d3.csv(
     min_life = d3.min(rows, function(d){ return d.life; });
     max_life = d3.max(rows, function(d){ return d.life; });
 
+    x = d3.scale.log().range([0,width]).domain([min_life, max_life]);
+    y = d3.scale.linear().range([height,0]).domain([min_literacy, max_literacy]);
+    radius_scale = d3.scale.sqrt().domain([0,1.3e9]).range([0,50]);
+
     console.log("min literacy rate: " + min_literacy);
     console.log("max literacy rate: " + max_literacy);
     console.log("min life expectancy: " + min_life);
     console.log("max life expectancy: " + max_life);
-      
+
     // now that the data is parsed, we can draw it
     draw(rows);
   }
 );
 
 // scale factors
-var x = d3.scale.log().range([0,width]);
-var y = d3.scale.linear().range([height,0]);
 
-// create axes
-var xAxis = d3.svg.axis()
+
+
+
+
+
+ function draw(rows){
+
+  // create axes
+  var xAxis = d3.svg.axis()
     .scale(x)
-    .tickSize(-height,0) //second argument suppresses domain path
+    .tickSize(-height, 0) //second argument suppresses domain path
     .tickFormat(d3.format(",d"))
-    .tickValues([35, 40, 45, 50, 55, 60, 65, 70, 75, 80])
     .orient("bottom");
-var yAxis = d3.svg.axis()
+
+  var yAxis = d3.svg.axis()
     .scale(y)
     .tickSize(5,0)
     .orient("left");
 
 
- function draw(rows){
-  //plotting the points
-  for(var i=0; i < rows.length; i++){
-    if (rows[i].life != undefined && rows[i].literacy != undefined){
-      svg.append('circle')
-        .attr('class','country')
-        .attr('cx', rows[i].life / max_life * width)
-        .attr('cy', height - rows[i].literacy / max_literacy * height)
-        .attr('r', 5) // put in an interesting scale here to make cirlces diff sizes
+    var countries = svg.selectAll('.country').data(rows, function(d){ return d.country; });
+
+    countries.enter()
+        .append('g')
+        .attr('class', function(d){ return d.country + " country"; });
+    
+    // then filter out the ones that have undefined data (we don't want to plot those points)
+    countries.filter(function(d){
+            return (d.life != undefined && d.literacy != undefined);
+        })
+        // .sort(function(a,b){
+        //     //puts bigger circles at the bottom
+        //     return b.pop - a.pop;
+        // })
+        .attr('transform',function(d){
+            return "translate(" + x(d.life) + "," + y(d.literacy) + ")";
+        })
+        .append('circle')
         .style('fill', '#21AD94')
+        .attr('r', function(d){ return radius_scale(d.pop); })
         .append('title')
-        .text(rows[i].country + ": " + rows[i].literacy);
-      }
-  }
+        .text( function(d){ return d.country + ": " + d.life});
 
   //drawing the axes
   svg.append('g')
